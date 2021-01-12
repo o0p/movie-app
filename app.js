e3e509945f7b0253bd8e5d7c8c46627e2e4f3e3d
Russell <russelldev@protonmail.com>
2021-01-12 11:49:47 +0000

    Add search funct

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=11dbf833cc3f6316214d045528716be0';
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1289';
-const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=11dbf833cc3f6316214d045528716be0&query="';
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=11dbf833cc3f6316214d045528716be0&query="';
+const form = document.getElementById('form');
+const search = document.getElementById('search');
 
 //get initial movies
 getMovies(API_URL);
@@ -9,3 +11,16 @@ async function getMovies(url) {
     const data = await res.json();
     console.log(data.results)
 }
+
+form.addEventListener('submit', function(evenet) {
+    evenet.preventDefault();
+
+    const searchTemrm = search.value;
+
+    if (searchTemrm && searchTemrm !=='') {
+        getMovies(SEARCH_API + searchTemrm);
+        searh.value = ''; 
+    } else {
+        window.location.reload();
+    }
+});
